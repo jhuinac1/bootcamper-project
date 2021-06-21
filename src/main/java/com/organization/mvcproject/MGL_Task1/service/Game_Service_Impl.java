adb06d148d14a813c863de97faf8013fc19bcefc
jhuinac1 <jhuinac7@gmail.com>
2021-06-21 12:57:17 -0700

    End of task 2, Created file for DAO and implemented it. saveGame() methods needs refactoring (implement a new method -update- to use best practice )

--- src/main/java/com/organization/mvcproject/MGL_Task1/service/Game_Service_Impl.java
+++ src/main/java/com/organization/mvcproject/MGL_Task1/service/Game_Service_Impl.java
@@ -3,79 +3,51 @@ package com.organization.mvcproject.MGL_Task1.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.organization.mvcproject.MGL_Task1.model.Game;
+import com.organization.mvcproject.dao.MockDAO;
 
 @Service("javaGameService")
 public class Game_Service_Impl implements Game_Service {
 
-	private static Long gameId = new Long(0);
-	private static Long companyId = new Long(0);
-	private static List<Game> games = new ArrayList<Game>();
-
-	static {
-		games = populateGames();
-	}
-
-	private static List<Game> populateGames() {
-
-		Game game1 = new Game();
-		game1.setGameId(++gameId);
-		game1.setGameGenre("Sport");
-		game1.setGameName("Rocket League");
-
-		Game game2 = new Game();
-		game2.setGameId(++gameId);
-		game2.setGameGenre("Shooter");
-		game2.setGameName("Halo 3");
-
-		Game game3 = new Game();
-		game3.setGameId(++gameId);
-		game3.setGameGenre("MMORPG");
-		game3.setGameName("Runescape");
-
-		games.add(game1);
-		games.add(game2);
-		games.add(game3);
-
-		return games;
-	}
+	@Autowired
+	private MockDAO gameDAO;
+	
 
 	@Override
 	public List<Game> retrieveAllGames() {
-		return games;
+		return gameDAO.retrieveAllGames();
 	}
-
+	
 	@Override
 	public Game saveGame(Game game) {
-		game.setGameId(++gameId);
-		games.add(game);
-		return game;
+		return gameDAO.saveGame(game);
 	}
-
-	public static Long getGameId() {
-		return gameId;
-	}
-
-	public static void setGameId(Long gameId) {
-		Game_Service_Impl.gameId = gameId;
-	}
-
-	public static Long getCompanyId() {
-		return companyId;
-	}
-
-	public static void setCompanyId(Long companyId) {
-		Game_Service_Impl.companyId = companyId;
-	}
-
-	public static List<Game> getGames() {
-		return games;
-	}
-
-	public static void setGames(ArrayList<Game> games) {
-		Game_Service_Impl.games = games;
+	
+	public boolean deleteGame(Game game) {
+		return gameDAO.deleteGame(game);
 	}
 
 }
+
+
+
+
+//
+//public static Long getGameId() {
+//	return gameId;
+//}
+//
+//public static void setGameId(Long gameId) {
+//	Game_Service_Impl.gameId = gameId;
+//}
+
+//public static Long getCompanyId() {
+//	return companyId;
+//}
+
+//public static void setCompanyId(Long companyId) {
+//	Game_Service_Impl.companyId = companyId;
+//}
---
+++ src/main/java/com/organization/mvcproject/dao/MockDAO.java
@@ -0,0 +1,97 @@
+package com.organization.mvcproject.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.organization.mvcproject.MGL_Task1.model.Game;
+
+
+@Repository
+public class MockDAO {
+	private static Long gameId = new Long(0);
+	private static Long companyId = new Long(0);
+	private static List<Game> games = new ArrayList<Game>();
+
+	static {
+		games = populateGames();
+	}
+
+	private static List<Game> populateGames() {
+
+		Game game1 = new Game();
+		game1.setGameId(++gameId);
+		game1.setGameGenre("Sport");
+		game1.setGameName("Rocket League");
+
+		Game game2 = new Game();
+		game2.setGameId(++gameId);
+		game2.setGameGenre("Shooter");
+		game2.setGameName("Halo 3");
+
+		Game game3 = new Game();
+		game3.setGameId(++gameId);
+		game3.setGameGenre("MMORPG");
+		game3.setGameName("Runescape");
+
+		games.add(game1);
+		games.add(game2);
+		games.add(game3);
+
+		return games;
+	}
+
+	//Read
+	public List<Game> retrieveAllGames() {
+		return games;
+	}
+
+	//Update Or Create
+	public Game saveGame(Game game) {		
+		
+		if(game.getGameId() != null) {
+			Game gameIsFound = findGameById(game.getGameId());
+			if(gameIsFound != null) {
+				for(int i = 0; i < games.size(); i++) {
+					if(gameIsFound.getGameId().equals(games.get(i).getGameId())) {
+						games.set(i, gameIsFound);
+						return gameIsFound;
+					}
+				}
+			}
+		}
+		
+		game.setGameId(++gameId);
+		games.add(game);
+		return game;
+	}
+	
+	
+	
+
+	public Game findGameById(Long gameId) {
+		for(Game game: games) {
+			if(game.getGameId().equals(gameId)) {
+				return game;
+			}
+		}
+		return null;
+	}
+	
+	public boolean deleteGame(Game game) {
+		Game gameIsFound = findGameById(game.getGameId());
+		
+		
+		if(gameIsFound != null) {
+			for(int i = 0; i < games.size(); i++) {
+				if(gameIsFound.getGameId().equals(games.get(i).getGameId())) {
+					games.remove(i);
+					return true;
+				}
+			}
+		}
+				
+		return false;
+	}
+}
